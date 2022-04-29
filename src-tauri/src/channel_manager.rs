a57039f19bf27406479d05348c49284a0219a801
mikoto2000 <mikoto2000@gmail.com>
2022-04-29 14:45:06 +0900

    Rust 側にチャンネル情報挿入メソッドを実装。

--- src-tauri/src/channel_manager.rs
+++ src-tauri/src/channel_manager.rs
@@ -10,16 +10,38 @@ pub fn get_channels() -> Vec<Channel> {
     channel.load::<Channel>(&conn).expect("Error loading posts")
 }
 
+pub fn insert_channel(uri: String, name: String) -> Result<usize, String> {
+    use crate::schema::channel;
+    use crate::model::NewChannel;
+    use crate::sqlite3::establish_connection;
+
+    let conn = establish_connection();
+
+    let new_channel = NewChannel {
+        uri: uri.as_str(),
+        name: name.as_str()
+    };
+
+    let insert_result = diesel::insert_into(channel::table)
+        .values(new_channel)
+        .execute(&conn);
+
+    match insert_result {
+        Err(why) => Err(why.to_string().into()),
+        Ok(insert_row_count) => Ok(insert_row_count)
+    }
+}
+
+
 #[cfg(test)]
 mod main_tests {
     use std::process::Command;
-    use crate::channel_manager::get_channels;
+    use crate::channel_manager::*;
 
     const LATEST_MIGRATION_DIR: &str = "./migrations/2022-04-29-021610_create_scab-player/";
 
     fn before() {
         clear_db();
-        run_sql_from_file("./test/channel_manager/before.sql", "./assets/scab-player.db");
     }
 
     fn after() {
@@ -29,6 +51,8 @@ mod main_tests {
     #[test]
     fn test_get_channels() {
         before();
+
+        run_sql_from_file("./test/channel_manager/before.sql", "./assets/scab-player.db");
 
         let channels = get_channels();
 
@@ -40,6 +64,24 @@ mod main_tests {
 
         after();
     }
+
+    #[test]
+    fn test_insert_channel() {
+        before();
+
+        let insert_count = insert_channel("insert_uri".to_string(), "insert_name".to_string());
+        assert_eq!(insert_count.unwrap(), 1);
+
+        let channels = get_channels();
+        let first_channel = channels.first().unwrap();
+
+        assert_eq!(first_channel.id, 1);
+        assert_eq!(first_channel.uri, "insert_uri");
+        assert_eq!(first_channel.name, "insert_name");
+
+        after();
+    }
+
 
     fn clear_db() {
         run_sql_from_file((LATEST_MIGRATION_DIR.to_string() + "down.sql").as_str(), "./assets/scab-player.db");
--- src-tauri/src/model.rs
+++ src-tauri/src/model.rs
@@ -1,6 +1,9 @@
 use serde::Deserialize;
 use serde::Serialize;
 
+use crate::schema::channel;
+
+
 #[derive(Serialize, Deserialize, Queryable, Debug)]
 pub struct Channel {
     pub id: i32,
@@ -8,3 +11,9 @@ pub struct Channel {
     pub name: String
 }
 
+#[derive(Insertable)]
+#[table_name = "channel"]
+pub struct NewChannel<'a> {
+    pub uri: &'a str,
+    pub name: &'a str
+}
