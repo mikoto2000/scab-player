2331c013f039de0fe672ecdae702e6f44881d630
mikoto2000 <mikoto2000@gmail.com>
2024-06-30 22:44:11 +0900

    不要な初期処理を削除。

--- src-tauri/src/app_initializer.rs
+++ src-tauri/src/app_initializer.rs
@@ -1,12 +1,9 @@
-use std::fs::{copy, create_dir_all};
-use std::path::PathBuf;
+use std::fs::{create_dir_all};
 
 use tauri::App;
 
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
-
-const DB_NAME : &str = "scab-player.db";
 
 pub fn init_db(app : &App) {
     let app_cache_dir = app.path_resolver().app_cache_dir().unwrap();
@@ -17,24 +14,8 @@ pub fn init_db(app : &App) {
     create_dir_all(episode_cache_dir)
         .expect("エピソードキャッシュ用ディレクトリの作成に失敗しました。");
 
-    let db_path : PathBuf = app_cache_dir.join(DB_NAME);
+    use crate::sqlite3::establish_connection;
+    let mut conn = establish_connection();
+    let _ = conn.run_pending_migrations(MIGRATIONS);
 
-    println!("db_path: {:#?}", db_path);
-
-    if db_path.is_file() {
-      // db_path に DB が見つかった場合、マイグレーション
-      use crate::sqlite3::establish_connection;
-      let mut conn = establish_connection();
-      let _ = conn.run_pending_migrations(MIGRATIONS);
-
-      return;
-    }
-
-    // db_path に db が見つからない場合、初期 DB ファイルをコピー
-    let original_db = PathBuf::from("assets/scab-player.db");
-    if original_db.is_file() {
-      println!("original_db: {:#?}", original_db);
-      copy(original_db, db_path)
-          .expect("initial db copy error.");
-    }
 }
