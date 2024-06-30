7b44e3b36ca82f3532277e06878079d75baf48ec
mikoto2000 <mikoto2000@gmail.com>
2024-06-30 14:19:30 +0900

    eslint 対応 その2.

--- src/EpisodeListItem.tsx
+++ src/EpisodeListItem.tsx
@@ -22,7 +22,7 @@ export function EpisodeListItem(props : EpisodeListItemProps) {
     if (props.episode.cache_uri) {
       setDownloaded(true);
     }
-  }, []);
+  }, [props.episode.cache_uri]);
 
   function getEpisodeIcon(episode : Episode) {
 
