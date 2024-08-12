2f539505318711baddc285c9b3ac0c5499ce552f
mikoto2000 <mikoto2000@gmail.com>
2024-08-12 02:16:47 +0000

    削除確認ダイアログを表示するように修正。

--- src/ChannelList.tsx
+++ src/ChannelList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import './ChannelList.css';
 import { Channel } from './CommonAppTypes'
@@ -6,26 +6,53 @@ import { Channel } from './CommonAppTypes'
 type ChannelListProps = {
   channels: Array<Channel>,
   onChannelClick: (channel_index: number) => void,
-  onChannelDeleteClick: (channel_index: number, event : React.MouseEvent<HTMLElement>) => void
+  onChannelDeleteClick: (channel_index: number, event: React.MouseEvent<HTMLElement>) => void
 };
 
-export function ChannelList(props : ChannelListProps) {
+export function ChannelList(props: ChannelListProps) {
+  const dialog = useRef<HTMLDialogElement | null>(null);
+
+  //   const [showConfirmDialog, setShowConfirmDialog] = useState<boolean>(false);
 
   const channelList = props.channels.map((channel, channel_index) => {
-    return <li
-        key={channel.uri}
-        onClick={() => props.onChannelClick(channel_index)}
+    return (
+      <>
+        <li
+          key={channel.uri}
+          onClick={() => props.onChannelClick(channel_index)}
         >
-        <label>{channel.name}</label><button onClick={(event) => props.onChannelDeleteClick(channel_index, event)}>削除</button></li>
+          <label>{channel.name}</label>
+          <button onClick={(event) => {
+            event.stopPropagation();
+            if (dialog && dialog.current) {
+              dialog.current.showModal();
+            }
+          }}>
+            削除
+          </button>
+        </li>
+        <dialog
+          ref={dialog}
+        >
+          <p>エピソードを削除しますか？</p>
+          <div>
+            <button onClick={() => { if (dialog && dialog.current) { dialog.current.close() } }}>キャンセル</button>
+            <button onClick={(event) => props.onChannelDeleteClick(channel_index, event)}>削除</button>
+          </div>
+        </dialog>
+      </>
+    )
   });
 
   return (
-    <div className="ChannelList">
-      <h1>Subscribed channels:</h1>
-      <ul>
-        {channelList}
-      </ul>
-    </div>
+    <>
+      <div className="ChannelList">
+        <h1>Subscribed channels:</h1>
+        <ul>
+          {channelList}
+        </ul>
+      </div>
+    </>
   );
 }
 
